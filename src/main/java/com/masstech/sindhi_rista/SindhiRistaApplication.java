2f5035326853c6b7aa1c51616967e51443043c52
S10may1999 <sandeepsharma89244@gmail.com>
2025-02-25 11:59:31 +0530

    this is sidhi rista project

---
+++ src/main/java/com/masstech/sindhi_rista/SindhiRistaApplication.java
@@ -0,0 +1,13 @@
+package com.masstech.sindhi_rista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SindhiRistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SindhiRistaApplication.class, args);
+	}
+
+}
